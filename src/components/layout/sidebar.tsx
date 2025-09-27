4773e2eb6cdfb04e35ad5725a4bed6d401fffb37
John Russelle Elleran <jrelleran@modularmajestics.com>
2025-09-27 06:28:05 +0000

    Try fixing this error: `Runtime Error: Error: useMemo is not defined. Er

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -3,7 +3,7 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import {
   BarChart,
   Home,
